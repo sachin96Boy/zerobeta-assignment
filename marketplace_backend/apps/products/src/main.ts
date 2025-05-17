61b3e4cd18dd5628718243415471728fda29a276
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-05-17 14:26:20 +0530

    chor: impl product ms

--- marketplace_backend/apps/products/src/main.ts
+++ marketplace_backend/apps/products/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ProductsModule } from './products.module';
+import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProductsModule);
-  await app.listen(process.env.port ?? 3000);
+
+  const connfigService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: connfigService.get<number>('PORT') ?? 3003,
+    },
+  });
+
+  await app.startAllMicroservices();
 }
 bootstrap();
--- marketplace_backend/apps/products/src/products.module.ts
+++ marketplace_backend/apps/products/src/products.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    LoggerModule,
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
