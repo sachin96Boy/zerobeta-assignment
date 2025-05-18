880d08f0681e76d18c0ceaf78b34182fa124d546
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-05-18 16:05:07 +0530

    chor: min chages

--- marketplace_backend/apps/marketplace_backend_gateway/src/main.ts
+++ marketplace_backend/apps/marketplace_backend_gateway/src/main.ts
@@ -4,9 +4,21 @@ import { ValidationPipe } from '@nestjs/common';
 
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MarketPlace example')
+    .setDescription('Marketplace API description')
+    .setVersion('1.0')
+    .addTag('market')
+    .addTag('sachin96boy')
+    .addTag('store')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(
     new ValidationPipe({
